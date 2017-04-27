258d58c088ba143961896832f1df81b6182cd734
Yuichi Masuda <1995.phoenix@gmail.com>
2017-04-27 15:36:32 +0900

    Initial Commit

---
+++ youtube_iframe_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtube_iframe_sample
+//
+//  Created by masudayuichi on 2017/04/27.
+//  Copyright © 2017年 masudayuichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ youtube_iframe_sampleTests/youtube_iframe_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  youtube_iframe_sampleTests.swift
+//  youtube_iframe_sampleTests
+//
+//  Created by masudayuichi on 2017/04/27.
+//  Copyright © 2017年 masudayuichi. All rights reserved.
+//
+
+import XCTest
+@testable import youtube_iframe_sample
+
+class youtube_iframe_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
